6132161bb87de85eb36d662182e96a3c8a42b108
Mohammad Rehan Multani <135141111+Rehan-Multani@users.noreply.github.com>
2024-05-02 14:40:45 +0530

    Update UserRoute.js

--- Routers/UserRoute.js
+++ Routers/UserRoute.js
@@ -1,12 +1,15 @@
 const express = require("express");
+const multer = require("multer"); // For handling file uploads
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 const {
     loginAdmin,
     SignupUser
 } = require("../Controllers/UserCtrl");
-
+const { uploadSingleImageToCloudinary } = require('../Middewares/singleImgUpload')
 const router = express.Router();
 
 router.post("/login", loginAdmin);
-router.post("/register", SignupUser);
+router.post("/register", upload.single('image'), uploadSingleImageToCloudinary, SignupUser);
 
 module.exports = router;
